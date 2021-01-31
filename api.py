a27c917ee6ca1413fcea6668bc5a54d743b5546a
Facundo Argana <efacundoargana@gmail.com>
2021-01-31 17:44:54 -0300

    Added certificate and change JSON schema

--- api.py
+++ api.py
@@ -9,8 +9,10 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route('/')
 def index():
+    url_cv_pdf = request.host_url + 'static/cv_facundo_argana.pdf'
     info = {
         'mensaje': 'Bienvenido/a! Esta es la API del CV de Facundo Argaña.',
+        'PDF': url_cv_pdf,
         'acciones': [
             'GET /curriculum',
             'POST /mensajes'
@@ -21,33 +23,103 @@ def index():
 
 @app.route('/curriculum', methods=['GET'])
 def cv():
-    url_imagen = request.host_url + 'static/profile-pic.jpg'
+    url_picture = request.host_url + 'static/profile-pic.jpg'
+    url_certificate = request.host_url + 'static/facundo-argana-ds-certiificate.pdf'
+
     cv = {
-        'nombre': 'Facundo',
-        'apellido': 'Argaña',
-        'residencia': 'Argentina',
-        'experiencia': [{
-            'posicion': 'Technical Solutions Engineer, Data',
-            'empresa': 'MightyHive',
-            'desde': 'marzo 2020',
-            'hasta': 'actualmente trabajo aquí',
-            'descripción': 'Diseñar, realizar y validar implementaciones de recolección de datos en sitios / apps, Diseñar y desarrollar aplicaciones de integraciones de datos en entornos Cloud, Coordinar y planificar proyectos de clientes, Desarrollar herramientas de uso interno / externo para validación, exploración y seguimiento de datos, Investigar nuevas herramientas y prácticas en torno a data analytics.'
-        }],
-        'educación': {
-            'nivel': 'Universitario',
-            'estado': 'incompleto',
-            'titulo': 'Licenciatura en Ciencias de Datos',
-            'institución': 'Universidad de Bunenos Aires',
-            'facultad': 'Facultad de Ciencias Exactas y Naturales'
+        'basics': {
+            'name': 'Facundo Argaña',
+            'label': 'Programador',
+            'picture': url_picture,
+            'email': 'efacundoargana@gmail.com',
+            'phone': '+54 9 11 6536 6254',
+            'summary': 'Curious and enthusiastic. Interested in the construction of computational models to explain and predict complex behaviors from data analysis. I am currently studying for a degree in Data Science at the University of Buenos Aires. I would like to work in a creative and interdisciplinary place. But above all, I like to work with complex problems that allow me to continue learning.',
+            'location': {
+                'address': 'Pareja 2470',
+                'postalCode': '1419',
+                'city': 'Ciudad Autónoma de Buenos Aires'
+            },
+            'profiles': [
+                {
+                    'network': 'GitHub',
+                    'username': 'efacundoargana',
+                    'url': 'https://github.com/efacundoargana'
+                },
+                {
+                    'network': 'LinkedIn',
+                    'username': 'efacundoargana',
+                    'url': 'https://www.linkedin.com/in/efacundoargana/'
+                },
+                {
+                    'network': 'Twitter',
+                    'username': 'efacundoargana',
+                    'url': 'https://twitter.com/efacundoargana'
+                },
+            ],
         },
-        'intereses': ['Data Science', 'Machine Learning', 'Computer Science'],
-        'redes': {
-            'github': 'https://github.com/efacundoargana',
-            'twitter': 'https://twitter.com/efacundoargana',
-            'linkedin': 'https://www.linkedin.com/in/efacundoargana/',
-            'email': 'efacundoargana@gmail.com'
-        },
-        'foto': url_imagen
+        'work': [
+            {
+                'name': 'MightyHive',
+                'location': 'Ciudad Autónoma de Buenos Aires',
+                'description': 'MightyHive is a new breed of media consultancy that partners with global brands and agencies seeking transformative marketing results in a time of massive disruption and opportunity. Recognized as a global leader in advanced marketing and advertising technologies, MightyHive provides consulting and services in the areas of media operations and training, data strategy and analytics. The company is headquartered in San Francisco, with teams in 19 countries and 24 cities around the world. In 2018, MightyHive merged with S4Capital plc (SFOR.L), a new age/new era digital advertising and marketing services company established by Sir Martin Sorrell in 2018.',
+                'url': 'https://mightyhive.com/',
+                'position': 'Technical Solutions Engineer, Data',
+                'startDate': '2019-12-01',
+                'endDate': 'Present',
+                'description': [
+                    'Design, perform and validate data collection implementations on sites / apps',
+                    'Design and develop data integration applications in Cloud environments',
+                    'Coordinate and plan client projects',
+                    'Develop internal / external use tools for data validation, exploration and monitoring',
+                    'Investigate new tools and practices around data analytics'
+                ],
+            }
+        ],
+        'education': [
+            {
+                'institution': 'Universidad de Buenos Aires',
+                'faculty': 'Facultad de Ciencias Exactas y Naturales',
+                'degree': 'Licenciatura en Ciencia de Datos',
+                'url': 'https://lcd.exactas.uba.ar/',
+                'startDate': '2020-03-21',
+                'endDate': 'In progress'
+            },
+            {
+                'certifications': [
+                    {
+                        'institution': 'Acámica',
+                        'title': 'Data Science Certification',
+                        'url': 'https://www.acamica.com/data-science',
+                        'certificate': url_certificate
+                    }
+                ]
+            }
+        ],
+        'skills': [
+            {
+                'name': 'Programming',
+                'languages': [
+                    'Pyhon',
+                    'SQL',
+                    'Javascript'
+                ]
+            }
+        ],
+        'languages': [
+            {
+                'language': 'English',
+                'fluency': 'advanced'
+            },
+            {
+                'language': 'Spanish',
+                'fluency': 'Native Speaker'
+            }
+        ],
+        'interests': [
+            'Data Science',
+            'Machine Learning',
+            'Computer Science'
+        ]
     }
     return jsonify(cv)
 
